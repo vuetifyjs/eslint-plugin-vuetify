bf34fe7834031061182578a0b37f715484f7f47c
Kael <kaelwd@gmail.com>
2019-07-15 11:11:53 +1000

    feat: add more grid props, improve messages

--- tests/lib/rules/no-legacy-grid.js
+++ tests/lib/rules/no-legacy-grid.js
@@ -17,27 +17,51 @@ tester.run('no-legacy-grid', rule, {
     {
       code: '<template><v-layout /></template>',
       output: '<template><v-row /></template>',
-      errors: ['Don\'t use deprecated grid components']
+      errors: [{ messageId: 'replacedWith' }]
     },
     {
       code: '<template><v-layout></v-layout></template>',
       output: '<template><v-row></v-row></template>',
-      errors: ['Don\'t use deprecated grid components']
+      errors: [{ messageId: 'replacedWith' }]
     },
     {
       code: '<template><v-layout row wrap /></template>',
-      output: '<template><v-row /></template>',
-      errors: ['Don\'t use deprecated grid components']
+      output: '<template><v-row   /></template>',
+      errors: [{ messageId: 'replacedWith' }, { messageId: 'removed' }, { messageId: 'removed' }]
+    },
+    {
+      code: '<template><v-layout justify-center /></template>',
+      output: '<template><v-row justify="center" /></template>',
+      errors: [{ messageId: 'replacedWith' }, { messageId: 'replacedWith' }]
     },
     {
       code: '<template><v-flex xs12 sm4 md3 /></template>',
       output: '<template><v-col cols="12" sm="4" md="3" /></template>',
-      errors: Array(4).fill('Don\'t use deprecated grid components')
+      errors: Array(4).fill({ messageId: 'replacedWith' })
     },
     {
       code: '<template><v-flex xs6 offset-sm1 /></template>',
       output: '<template><v-col cols="6" offset-sm="1" /></template>',
-      errors: Array(3).fill('Don\'t use deprecated grid components')
+      errors: Array(3).fill({ messageId: 'replacedWith' })
+    },
+    {
+      code: `<template>
+        <v-layout
+          fill-height
+          align-center
+          justify-center
+          ma-0
+        />
+      </template>`,
+      output: `<template>
+        <v-row
+          fill-height
+          align="center"
+          justify="center"
+          ma-0
+        />
+      </template>`,
+      errors: Array(3).fill({ messageId: 'replacedWith' })
     }
   ]
 })
