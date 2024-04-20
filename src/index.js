ccbfdb94b2b3ac6ec7bbb9a0758e7ed27af76fa3
Corbin Uselton <cuselton@altametrics.com>
2024-04-19 21:46:22 -0700

    Migrate plugin to eslint 9

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@
 const path = require('path')
 const requireindex = require('requireindex')
 
-module.exports = {
-  configs: requireindex(path.join(__dirname, './configs')),
+const configs = requireindex(path.join(__dirname, './configs'))
+
+const plugin = {
+  configs: {},
   rules: requireindex(path.join(__dirname, './rules')),
 }
+
+Object.assign(plugin.configs, {
+  recommended: {
+    plugins: {
+      vuetify: plugin,
+    },
+    rules: {
+      ...configs.recommended.rules,
+    },
+  },
+  base: {
+    plugins: {
+      vuetify: plugin,
+    },
+    rules: {
+      ...configs.base.rules,
+    },
+  },
+})
+
+module.exports = plugin
